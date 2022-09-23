8bac454529643aaf75124521e1b16c9ba6027c1f
Enokek <enok.ekelund@gmail.com>
2022-09-23 11:42:40 +0200

    Switched from two arrays to one multidimensional one

---
+++ public/app.js
@@ -0,0 +1,22 @@
+var computersum = 0;
+var yourSum = 0;
+
+var hidden;
+var deck;
+
+/*function buildDeck () {
+var number = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
+let cardType = ['C','H','S','D' ] Clubs, Hearts, Spades, Diamonds
+    
+*/
+
+var buildDeck = [
+[A,2,3,4,5,6,7,8,9,10,J,Q,K]
+[A,2,3,4,5,6,7,8,9,10,J,Q,K]
+[A,2,3,4,5,6,7,8,9,10,J,Q,K]
+[A,2,3,4,5,6,7,8,9,10,J,Q,K]
+
+]
+
+
+console.log(buildDeck[2][3]);
