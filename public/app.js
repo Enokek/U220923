d46aba6edaa643c9182f715068658cfdfbccd87c
Enokek <enok.ekelund@gmail.com>
2022-09-23 13:22:59 +0200

    Changed array values to x and y, defined them by var x and var y

--- public/app.js
+++ public/app.js
@@ -1,22 +1,16 @@
 var computersum = 0;
 var yourSum = 0;
 
-var hidden;
-var deck;
+
 
 /*function buildDeck () {
 var number = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
 let cardType = ['C','H','S','D' ] Clubs, Hearts, Spades, Diamonds
     
-*/
 
-var buildDeck = [
-[1,2,3,4,5,6,7,8,9,10,11,12,13]
-[1,2,3,4,5,6,7,8,9,10,11,12,13]
-[1,2,3,4,5,6,7,8,9,10,11,12,13]
-[1,2,3,4,5,6,7,8,9,10,11,12,13]
+// random x = 0..3, y = 0..12;
 
-];
+
 var CardCharacter = {
 
     Clubs: '',
@@ -25,13 +19,16 @@ var CardCharacter = {
     Diamonds: ''
 
 }
-  
+*/
 
+var buildDeck = [x, y];
+  
 console.log(buildDeck[2][3]);
 
 function draw(max) {
 return Math.floor(Math.random() * max);
-console.log(getRandomInt(3));
+
+console.log(draw(3));
 
 }
 
