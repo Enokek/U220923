559005e85652f165be4ff326597be60fc225da5b
Enokek <enok.ekelund@gmail.com>
2022-09-23 15:12:31 +0200

    Added functions for computer cards, and functions for multiple buttons

--- public/app.js
+++ public/app.js
@@ -3,36 +3,51 @@ var yourSum = 0;
 
 
 
-/*function buildDeck () {
-var number = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
-let cardType = ['C','H','S','D' ] Clubs, Hearts, Spades, Diamonds
-    
-
-// random x = 0..3, y = 0..12;
-
-
-var CardCharacter = {
-
-    Clubs: '',
-    Hearts: '',
-    Spades: '',
-    Diamonds: ''
-
-}
-*/
 var x;
 var y;
 var buildDeck = [x, y];
-  
-console.table()
+var gamestart = [];
+var YourCards = [];
 
-function drawCard(max) {
+
+
+function drawCards() {
+    const divMain = document.getElementById("main")
+    divMain.hidden = false;
+    
+    YourCards = [];
     var randomCard = (max) => Math.floor(Math.random(buildDeck) * max);
-    console.log(randomCard(4) + ', ' + randomCard(13));
-
-
-   
+    for (i=0; i < 2; i++) {
+    var Cards = (randomCard(4) + ', ' + randomCard(13));
+    YourCards.push(Cards);
+    }
+    console.log(YourCards);
+    
+    Computercard = document.getElementById("ComputerCards");
+    Computercard.innerHTML = '';
+    Computercard.innerHTML=(` Computers card are: ${YourCards} ` )
 }
 
+function hit (){
+    var randomCard = (max) => Math.floor(Math.random(buildDeck) * max);
+    var Cards = (randomCard(4) + ', ' + randomCard(13));
+    YourCards.push(Cards);
+    console.log(YourCards);
+    }
+    
+
+    
 
 
+/*var yourcards =[]
+
+function drawCard(max) {
+var randomCard = (max) => Math.floor(Math.random(buildDeck) * max);
+    console.log(randomCard(4) + ', ' + randomCard(13));
+
+ComputerCards.push(buildDeck)
+console.log(ComputerCards)
+
+}
+
+*/
