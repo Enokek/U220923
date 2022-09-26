69a90ec1433516ec1419bdc813e6c306fab32a97
Enok <enok.ekelund@gmail.com>
2022-09-26 10:37:44 +0200

    hit button adds new card to players card

--- public/app.js
+++ public/app.js
@@ -5,34 +5,51 @@ var yourSum = 0;
 
 var x;
 var y;
-var buildDeck = [x, y];
+var Deck = [color, value];
 var gamestart = [];
 var YourCards = [];
+var ComputerCards = [];
+var hitCards = [];
 
 
 
 function drawCards() {
     const divMain = document.getElementById("main")
     divMain.hidden = false;
-    
     YourCards = [];
-    var randomCard = (max) => Math.floor(Math.random(buildDeck) * max);
+    var randomCard = (max) => Math.floor(Math.random(Deck) * max);
+
     for (i=0; i < 2; i++) {
     var Cards = (randomCard(4) + ', ' + randomCard(13));
     YourCards.push(Cards);
     }
-    console.log(YourCards);
+
+    ComputerCards = [];
+    var randomCard = (max) => Math.floor(Math.random(Deck) * max);
+    for (i=0; i < 2; i++) {
+    var Cards = (randomCard(4) + ', ' + randomCard(13));
+    ComputerCards.push(Cards);
     
+    }
+
     Computercard = document.getElementById("ComputerCards");
     Computercard.innerHTML = '';
     Computercard.innerHTML=(` Computers card are: ${YourCards} ` )
+
+    playercard = document.getElementById("PlayerCards");
+    playercard.innerHTML = '';
+    playercard.innerHTML=(` Players card are: ${ComputerCards} ` )
+
 }
 
+ 
 function hit (){
-    var randomCard = (max) => Math.floor(Math.random(buildDeck) * max);
+    var randomCard = (max) => Math.floor(Math.random(Deck) * max);
     var Cards = (randomCard(4) + ', ' + randomCard(13));
-    YourCards.push(Cards);
-    console.log(YourCards);
+    hitCards.push(Cards);
+
+    hitCard = document.getElementById("hitcard")
+    hitCard.innerHTML = (`${hitCards}` )
     }
     
 
